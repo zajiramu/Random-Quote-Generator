2f1697cc392917ad33813bc6546688895665a435
umari <umarijaz3014@gmail.com>
2020-09-30 18:55:35 -0700

    added project 1 starter files to main repo folder

---
+++ js/script.js
@@ -0,0 +1,83 @@
+/******************************************
+Treehouse FSJS Techdegree:
+Project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * 'quotes' object array 
+ *  each quote object has two properties, the quote as a string & the source or person who stated the quote, also as a string
+***/
+
+const quotes = [
+  {
+      quote: "You know you’re in love when you can’t fall asleep because reality is finally better than your dreams",
+      source: "Dr. Seuss"
+  },
+  {
+      quote: "Get busy living or get busy dying",
+      source: "Stephen King"
+  },
+  {
+      quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do",
+      source: "Mark Twain"
+  },
+  {
+      quote: "Those who dare to fail miserably can achieve greatly",
+      source: "John F. Kennedy"
+  },
+  {
+      quote: "It is hard to fail, but it is worse never to have tried to succeed",
+      source: "Theodore Roosevelt"
+  },
+  {
+      quote: "If you want to live a happy life, tie it to a goal, not to people or things",
+      source: "Albert Einstein"
+  },
+  {
+      quote: "Never let the fear of striking out keep you from playing the game",
+      source: "Babe Ruth"
+  },
+  {
+      quote: "The successful warrior is the average man, with laser-like focus",
+      source: "Bruce Lee"
+  },
+  {
+      quote: "The successful warrior is the average man, with laser-like focus",
+      source: "Ray Bradbury"
+  },
+  {
+      quote: "Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present",
+      source: "Bill Keane"
+  },
+  {
+      quote: "You miss 100 percent of the shots you never take",
+      source: "Wayne Gretzky"
+  },
+  {
+      quote: "The whole secret of a successful life is to find out what is one’s destiny to do, and then do it",
+      source: "Henry Ford"
+  } ];
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
